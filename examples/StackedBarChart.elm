3b4e03275a8466afb562fd9bc925f5190cd59be2
Folkert de Vries <folkert@folkertdev.nl>
2017-07-29 20:42:38 +0200

    add new files for Stack

---
+++ examples/StackedBarChart.elm
@@ -0,0 +1,151 @@
+module StackedBarChart exposing (main)
+
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Color.Convert exposing (colorToCssRgb)
+import Visualization.Axis as Axis exposing (defaultOptions)
+import Visualization.Shape as Shape
+import Visualization.Scale as Scale exposing (BandConfig, BandScale, ContinuousScale, defaultBandConfig)
+import SampleData exposing (CrimeRate)
+import Stack exposing (StackConfig, StackResult)
+import Color exposing (Color)
+import List.Extra as List
+
+
+type alias Year =
+    Int
+
+
+series : List { label : String, accessor : CrimeRate -> Int }
+series =
+    [ { label = "Assault"
+      , accessor = .assault
+      }
+    , { label = "Rape"
+      , accessor = .rape
+      }
+    , { label = "Robbery"
+      , accessor = .robbery
+      }
+    , { label = "Murder"
+      , accessor = .murder
+      }
+    ]
+
+
+samples : List ( String, List Float )
+samples =
+    List.map (\{ label, accessor } -> ( label, List.map (toFloat << accessor) SampleData.crimeRates )) series
+
+
+canvas : { width : Float, height : Float }
+canvas =
+    { width = 900
+    , height = 450
+    }
+
+
+padding : { bottom : number, left : number1, right : number2, top : number3 }
+padding =
+    { top = 30
+    , left = 60
+    , right = 30
+    , bottom = 60
+    }
+
+
+config : StackConfig String
+config =
+    { data = samples
+    , offset = Shape.stackOffsetNone
+    , order =
+        -- stylistic choice: largest (by sum of values) category at the bottom
+        List.sortBy (Tuple.second >> List.sum >> negate)
+    }
+
+
+sampleColor : Float -> Color
+sampleColor progression =
+    -- stylistic choice: the larger boxes look better in brighter colors, so invert the interpolator
+    Scale.viridisInterpolator (1 - progression)
+
+
+colors : Int -> List String
+colors size =
+    let
+        -- given an index, give a value in [0,1] (0 for first, 1 for final)
+        lengthScale =
+            Scale.linear ( 0, toFloat size - 1 ) ( 0, 1 )
+                |> Scale.convert
+    in
+        List.range 0 (size - 1)
+            |> List.map (colorToCssRgb << sampleColor << lengthScale << toFloat)
+
+
+column : BandScale Year -> ( Year, List ( Float, Float ) ) -> Svg msg
+column xScale ( year, values ) =
+    let
+        block color ( upperY, lowerY ) =
+            rect
+                [ x <| toString <| Scale.convert xScale year
+                , y <| toString <| lowerY
+                , width <| toString <| Scale.bandwidth xScale
+                , height <| toString <| (abs <| upperY - lowerY)
+                , fill color
+                ]
+                []
+    in
+        g [ class "column" ] (List.map2 block (colors (List.length values)) values)
+
+
+view : StackResult String -> Svg msg
+view { values, labels } =
+    let
+        -- transpose back to get the values per year
+        yearValues =
+            List.transpose values
+
+        years =
+            List.map .year SampleData.crimeRates
+
+        xScale : BandScale Year
+        xScale =
+            Scale.band { defaultBandConfig | paddingInner = 0.1, paddingOuter = 0.2 } years ( 0, canvas.width - (padding.top + padding.bottom) )
+
+        yScale : ContinuousScale
+        yScale =
+            Scale.linear (Stack.calculateExtremes values) ( canvas.height - (padding.left + padding.right), 0 )
+                |> flip Scale.nice 4
+
+        axisOptions =
+            Axis.defaultOptions
+
+        xAxis : Svg msg
+        xAxis =
+            Axis.axis { axisOptions | orientation = Axis.Bottom, tickCount = 10 } (Scale.toRenderable xScale)
+
+        yAxis : Svg msg
+        yAxis =
+            Axis.axis { axisOptions | orientation = Axis.Left {- , ticks = Just () -} } yScale
+
+        scaledValues =
+            List.map (List.map (\( y1, y2 ) -> ( Scale.convert yScale y1, Scale.convert yScale y2 ))) yearValues
+    in
+        svg [ width (toString canvas.width ++ "px"), height (toString canvas.height ++ "px") ]
+            [ g [ translate (padding.left - 1) (canvas.height - padding.bottom) ]
+                [ xAxis ]
+            , g [ translate (padding.left - 1) padding.top ]
+                [ yAxis ]
+            , g [ translate padding.left padding.top, class "series" ] <|
+                List.map (column xScale) (List.map2 (,) years scaledValues)
+            ]
+
+
+translate : number -> number -> Svg.Attribute msg
+translate x y =
+    transform ("translate(" ++ toString x ++ ", " ++ toString y ++ ")")
+
+
+main : Svg msg
+main =
+    view (Stack.create config)
--- examples/Streamgraph.elm
+++ examples/Streamgraph.elm
@@ -2,31 +2,20 @@ module Streamgraph exposing (..)
 
 import SampleData exposing (CrimeRate)
 import Color.Convert exposing (colorToCssRgb)
-import Visualization.Shape as Shape
-import Visualization.Scale as Scale exposing (ContinuousScale, OrdinalScale)
+import Visualization.Scale as Scale exposing (ContinuousScale, OrdinalScale, BandScale, defaultBandConfig, Scale)
 import Visualization.Axis as Axis exposing (Orientation(..))
-import Path exposing (Path, moveTo, subpath, lineTo, closePath)
 import List.Extra as List
-import Visualization.List exposing (extent)
 import Curve
 import Color exposing (Color)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Stack exposing (StackConfig, StackResult)
+import Visualization.Shape
+import Path exposing (Path)
 
 
-w : Float
-w =
-    900
-
-
-h : Float
-h =
-    450
-
-
-padding : Float
-padding =
-    60
+type alias Year =
+    Int
 
 
 series : List { label : String, accessor : CrimeRate -> Int }
@@ -46,20 +35,9 @@ series =
     ]
 
 
-accessors : List (CrimeRate -> Int)
-accessors =
-    List.map .accessor series
-
-
-values : CrimeRate -> List Float
-values i =
-    List.map (\a -> toFloat <| a i) accessors
-
-
 samples : List ( String, List Float )
 samples =
     List.map (\{ label, accessor } -> ( label, List.map (toFloat << accessor) SampleData.crimeRates )) series
-        |> Debug.log "samples"
 
 
 colorScale : OrdinalScale String Color
@@ -68,84 +46,126 @@ colorScale =
 
 
 sampleColor : String -> Color
-sampleColor =
-    Scale.convert colorScale >> Maybe.withDefault Color.black
+sampleColor label =
+    Scale.convert colorScale label
+        |> Maybe.withDefault Color.black
 
 
-main : Svg msg
-main =
-    view samples
-
-
-view : List ( String, List Float ) -> Svg a
-view data =
+colors : List String -> List Color
+colors labels =
     let
-        ( labels, stacked ) =
-            stack data
+        size =
+            List.length labels
 
-        labelPositions =
-            List.map (List.last >> Maybe.withDefault ( 0, 0 ) >> (\( y1, y2 ) -> (y2 + y1) / 2)) stacked
-                |> List.map (Scale.convert yScale)
+        -- given an index, give a value in [0,1] (0 for first, 1 for final)
+        lengthScale =
+            Scale.linear ( 0, toFloat size - 1 ) ( 0, 1 )
+                |> Scale.convert
+    in
+        List.map sampleColor labels
+
+
+canvas : { width : Float, height : Float }
+canvas =
+    { width = 900
+    , height = 450
+    }
+
+
+padding : { bottom : number, left : number1, right : number2, top : number3 }
+padding =
+    { top = 60
+    , left = 60
+    , right = 60
+    , bottom = 60
+    }
+
+
+stackedGraphConfig : StackConfig String
+stackedGraphConfig =
+    { data = samples
+    , offset = Visualization.Shape.stackOffsetNone
+    , order = List.sortBy (Tuple.second >> List.sum >> negate)
+    }
+
+
+streamgraphConfig : StackConfig String
+streamgraphConfig =
+    { data = samples
+    , offset = Visualization.Shape.stackOffsetWiggle
+    , order = Visualization.Shape.sortByInsideOut (Tuple.second >> List.sum)
+    }
+
+
+config : StackConfig String
+config =
+    streamgraphConfig
+
+
+view : StackResult String -> Svg msg
+view { values, labels } =
+    let
+        years : List Year
+        years =
+            List.map .year SampleData.crimeRates
+
+        xScale : BandScale Year
+        xScale =
+            Scale.band { defaultBandConfig | paddingInner = 0.1, paddingOuter = 0.2 } years ( 0, canvas.width - (padding.top + padding.bottom) )
+
+        yScale : ContinuousScale
+        yScale =
+            Scale.linear (Stack.calculateExtremes values) ( canvas.height - (padding.left + padding.right), 0 )
+                |> flip Scale.nice 4
 
         axisOptions =
             Axis.defaultOptions
 
-        extent =
-            SampleData.crimeRates
-                |> List.map (.year >> toFloat)
-                |> Visualization.List.extent
-                |> Maybe.withDefault ( 1900, 1901 )
-
-        internalXScale =
-            Scale.linear ( 0, uncurry (flip (-)) extent ) ( 0, w - 2 * padding )
-
-        xScale : ContinuousScale
-        xScale =
-            Scale.linear extent ( 0, w - 2 * padding )
-
-        ( minimal, maximal ) =
-            calculateExtremes stacked
-
-        yScale : ContinuousScale
-        yScale =
-            Scale.linear ( minimal, maximal ) ( h - 2 * padding, 0 )
-
-        --|> flip Scale.nice 4
         xAxis : Svg msg
         xAxis =
-            Axis.axis { axisOptions | orientation = Axis.Bottom, tickCount = 10 } xScale
+            -- TODO fewer ticks, tickCount doesn't seem to work
+            Axis.axis { axisOptions | orientation = Axis.Bottom, tickCount = 1 } (Scale.toRenderable xScale)
 
         yAxis : Svg msg
         yAxis =
             Axis.axis { axisOptions | orientation = Axis.Left {- , ticks = Just () -} } yScale
 
-        size =
-            List.length stacked
+        paths =
+            List.map2 (renderStream (Curve.catmullRom 1.0 << Stack.repeatFirst) ( xScale, yScale )) (colors labels) values
 
-        pathElements =
-            List.map2 (\( label, _ ) values -> renderStream ( internalXScale, yScale ) (Scale.convert colorScale label |> Maybe.withDefault Color.black) values) data stacked
+        labelPositions =
+            List.map (List.last >> Maybe.withDefault ( 0, 0 ) >> (\( y1, y2 ) -> (y2 + y1) / 2)) values
+                |> List.map (Scale.convert yScale)
     in
-        -- Svg.svg [ Svg.height (toString h), Svg.width (toString w) ] (xAxis :: pathElements)
-        Svg.svg [ width (toString w ++ "px"), height (toString h ++ "px") ]
-            [ g [ translate (padding - 1) (h - padding) ]
+        Svg.svg [ width (toString canvas.width ++ "px"), height (toString canvas.height ++ "px") ]
+            [ g [ translate (padding.left - 1) (canvas.height - padding.top) ]
                 [ xAxis ]
-            , g [ translate padding padding, class "series" ] pathElements
-            , g [ translate (padding - 1) padding ]
+            , g [ translate padding.left padding.top, class "series" ] paths
+            , g [ translate (padding.left - 1) padding.top ]
                 [ yAxis, text_ [ fontFamily "sans-serif", fontSize "10", x "5", y "5" ] [ text "Occurences" ] ]
             , g [ fontFamily "sans-serif", fontSize "10" ]
                 (List.map2
                     (\label yPosition ->
-                        g [ translate (w - padding + 10) (padding + yPosition) ]
+                        g [ translate (canvas.width - padding.right + 10) (padding.top + yPosition) ]
                             [ text_ [ fill (sampleColor label |> colorToCssRgb) ] [ text label ] ]
                     )
                     labels
                     labelPositions
                 )
-            , g [ translate (w - padding) (padding + 20) ]
+            , g [ translate (canvas.width - padding.right) (padding.top + 20) ]
                 [ text_ [ fontFamily "sans-serif", fontSize "20", textAnchor "end" ] [ text "Violent Crime in the US" ]
                 , text_ [ fontFamily "sans-serif", fontSize "10", textAnchor "end", dy "1em" ] [ text "Source: fbi.gov" ]
                 ]
             ]
+
+
+{-| Renders one colored stream with given scaling
+
+The result is a generator because we generate the  band color randomly
+-}
+renderStream : (List ( Float, Float ) -> Path) -> ( Scale { a | range : ( Float, Float ) }, ContinuousScale ) -> Color -> List ( Float, Float ) -> Svg msg
+renderStream curve scales color coords =
+    Path.svgPath (Stack.toArea curve scales coords) [ fill (colorToCssRgb color) ]
 
 
 translate : number -> number -> Svg.Attribute msg
@@ -153,139 +173,6 @@ translate x y =
     transform ("translate(" ++ toString x ++ ", " ++ toString y ++ ")")
 
 
-stack : List ( a, List Float ) -> ( List a, List (List ( Float, Float )) )
-stack items =
-    let
-        ( labels, values ) =
-            items |> Shape.sortByInsideOut (Tuple.second >> List.sum) |> List.unzip
-    in
-        values
-            |> List.map (List.indexedMap (\i e -> ( toFloat i, e )))
-            |> Shape.stackOffsetWiggle
-            |> (,) labels
-
-
-{-| Calculates the minimal and maximal y values, for correct scaling
--}
-calculateExtremes : List (List ( Float, Float )) -> ( Float, Float )
-calculateExtremes coords =
-    let
-        folder ( y1, y2 ) ( accmin, accmax ) =
-            ( Basics.min y1 y2 |> Basics.min accmin, Basics.max y1 y2 |> Basics.max accmax )
-    in
-        List.map (List.foldl folder ( 0, 0 )) coords
-            |> List.foldl (\( mi, ma ) ( accmin, accmax ) -> ( Basics.min mi accmin, Basics.max ma accmax )) ( 0, 0 )
-
-
-type alias Scales =
-    ( Float, Float ) -> ( Float, Float )
-
-
-{-| Calculate x and y scales - to make the paths fit the canvas width and height
--}
-calculateScales : { width : Float, height : Float } -> List (List ( Float, Float )) -> ( ContinuousScale, ContinuousScale )
-calculateScales { width, height } coords =
-    let
-        ( minimal, maximal ) =
-            calculateExtremes coords
-
-        -- the number of data points per label
-        sizeX =
-            List.head coords
-                |> Maybe.map List.length
-                |> Maybe.withDefault 0
-
-        scaleX =
-            Scale.linear ( 0, toFloat sizeX - 1 ) ( 0, width )
-
-        scaleY =
-            Scale.linear ( minimal, maximal ) ( height, 0 )
-    in
-        ( scaleX, scaleY )
-
-
-{-| Renders one colored stream with given scaling
-
-The result is a generator because we generate the  band color randomly
--}
-renderStream : ( ContinuousScale, ContinuousScale ) -> Color -> List ( Float, Float ) -> Svg msg
-renderStream scales color coords =
-    let
-        path =
-            toArea (Curve.catmullRom 1.0 << repeatFirst) scales coords
-
-        --toArea (Curve.linear) scales coords
-        constructPath fillColor =
-            Path.svgPath path [ fill fillColor ]
-    in
-        color
-            |> colorToCssRgb
-            |> constructPath
-
-
-repeatFirst : List a -> List a
-repeatFirst items =
-    case items of
-        [] ->
-            []
-
-        x :: xs ->
-            x :: x :: xs
-
-
-toArea : (List ( Float, Float ) -> Path) -> ( ContinuousScale, ContinuousScale ) -> List ( Float, Float ) -> Path
-toArea curve ( scaleX, scaleY ) ys =
-    let
-        order ( y1, y2 ) =
-            if y1 < y2 then
-                ( y1, y2 )
-            else
-                ( y2, y1 )
-
-        ( low_, high_ ) =
-            List.map order ys
-                |> List.unzip
-
-        scale ( x, y ) =
-            ( Scale.convert scaleX x
-            , Scale.convert scaleY y
-            )
-
-        floatTuple i e =
-            ( toFloat i, e )
-                |> scale
-
-        ( low, high ) =
-            ( List.indexedMap floatTuple low_, List.indexedMap floatTuple high_ )
-    in
-        cycle curve low high
-
-
-cycle : (List ( Float, Float ) -> Path) -> List ( Float, Float ) -> List ( Float, Float ) -> Path
-cycle curve low high_ =
-    let
-        high =
-            List.reverse high_
-    in
-        case ( low, high ) of
-            ( l :: ls, h :: hs ) ->
-                let
-                    lastLow =
-                        List.last ls |> Maybe.withDefault l
-
-                    connectorRight =
-                        lineTo [ lastLow, h ]
-
-                    connectorLeft =
-                        closePath
-                in
-                    case ( curve low, curve high ) of
-                        ( [ bottom ], [ top ] ) ->
-                            subpath (moveTo l) (bottom.drawtos ++ [ connectorRight ] ++ top.drawtos ++ [ connectorLeft ])
-                                |> List.singleton
-
-                        _ ->
-                            []
-
-            _ ->
-                []
+main : Svg msg
+main =
+    view (Stack.create config)
