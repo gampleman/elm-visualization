d4a29200f98faf2fe7d70346670bef06004af312
Jakub Hampl <kopomir@gmail.com>
2017-11-24 16:21:39 +0000

    Adds a scale formatting improvement

--- src/Visualization/Scale/Internal.elm
+++ src/Visualization/Scale/Internal.elm
@@ -1,4 +1,6 @@
-module Visualization.Scale.Internal exposing (bimap, interpolateFloat)
+module Visualization.Scale.Internal exposing (bimap, interpolateFloat, toFixed)
+
+import String exposing (padRight, split, join)
 
 
 bimap :
@@ -21,3 +23,28 @@ bimap ( d0, d1 ) ( r0, r1 ) deinterpolate reinterpolate =
 interpolateFloat : Float -> Float -> Float -> Float
 interpolateFloat from to time =
     from + (to - from) * time
+
+
+toFixed : Int -> Float -> String
+toFixed precision value =
+    let
+        power =
+            toFloat 10 ^ (toFloat precision)
+
+        pad num =
+            case num of
+                [ x, y ] ->
+                    [ x, String.padRight precision '0' y ]
+
+                [ val ] ->
+                    [ val, String.padRight precision '0' "" ]
+
+                val ->
+                    val
+    in
+        (round (value * power) |> toFloat)
+            / power
+            |> toString
+            |> String.split "."
+            |> pad
+            |> String.join "."
