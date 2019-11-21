63bee441ae271241875810ddb17e32595262502e
Jakub Hampl <kopomir@gmail.com>
2019-11-21 16:14:08 +0000

    Removes some cruft

--- Interpolation.elm
+++
@@ -1,8 +0,0 @@
-module Interpolation exposing (..)
-
-type Interpolation a =
-  Interpolation (a -> a ->Float ->  a)
-
-bimap : (a -> b) -> (b -> a) -> Interpolation a -> Interpolation b
-bimap fn parser (Interpolation interpolator) =
-  Interpolation (\from to param ->  fn (interpolator (parser from) (parser to) param) )
