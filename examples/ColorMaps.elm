26a7bbfb64998c882dc6be8e5d5f1aa902ba7ac4
Jakub Hampl <kopomir@gmail.com>
2020-07-01 17:16:25 +0200

    Fixes color map to fit nicely

--- examples/ColorMaps.elm
+++ examples/ColorMaps.elm
@@ -3,245 +3,48 @@ module ColorMaps exposing (main)
 {-| This module shows the color schemes available in the Scale.Color module
 
 @category Reference
+
 -}
 
 import Array
 import Browser
 import Color exposing (Color, rgb255)
 import Html exposing (Html, div)
-import Html.Attributes exposing (class, for, href, id, selected, style, type_, value)
+import Html.Attributes exposing (class, for, href, id, selected, style, title, type_, value)
 import Html.Events exposing (onInput)
 import Interpolation exposing (Interpolator)
 import Scale.Color
-    exposing
-        ( accent
-        , blueGreenInterpolator
-        , blueOrangeInterpolator
-        , bluePurpleInterpolator
-        , bluesInterpolator
-        , brownBlueGreenInterpolator
-        , brownsInterpolator
-        , carbonAlert
-        , carbonDiverging1Interpolator
-        , carbonDiverging2Interpolator
-        , category10
-        , colorblind
-        , goldGreensInterpolator
-        , goldOrangeInterpolator
-        , goldRedInterpolator
-        , greenBlueInterpolator
-        , greensInterpolator
-        , greysInterpolator
-        , lightGreyRedInterpolator
-        , lightGreyTealInterpolator
-        , lightMultiInterpolator
-        , lightOrangeInterpolator
-        , orangeRedInterpolator
-        , orangesInterpolator
-        , paired
-        , pastel1
-        , pastel2
-        , pinkYellowGreenInterpolator
-        , purpleBlueGreenInterpolator
-        , purpleBlueInterpolator
-        , purpleGreenInterpolator
-        , purpleOrangeInterpolator
-        , purpleRedInterpolator
-        , purplesInterpolator
-        , rainbowInterpolator
-        , redBlueInterpolator
-        , redGreyInterpolator
-        , redPurpleInterpolator
-        , redYellowBlueInterpolator
-        , redYellowGreenInterpolator
-        , redsInterpolator
-        , set1
-        , set2
-        , sinebowInterpolator
-        , spectralInterpolator
-        , tableau10
-        , tealBluesInterpolator
-        , tealInterpolator
-        , turboInterpolator
-        , warmGreysInterpolator
-        , yellowGreenBlueInterpolator
-        , yellowGreenInterpolator
-        , yellowOrangeBrownInterpolator
-        , yellowOrangeRedInterpolator
-        )
 
 
-css : String
-css =
-    """
-h1 {
-    font-size: 24px;
-}
-
-h2 {
-    font-size: 20px;
-    font-weight: normal;
-    margin: 30px 0 20px 0;
-}
-
-body {
-    font-family: Sans-Serif;
-}
-
-.wrapper {
-    margin: 25px;
-    width: 500px;
-    position: relative;
-}
-
-.palette {
-    display: flex;
-    width: 100%;
-    margin: 2px 0 10px;
-}
-
-.palette div {
-    height: 30px;
-    flex: 1;
-}
-"""
-
-
-turboColorMap : Html msg
-turboColorMap =
-    div []
-        [ div []
-            [ Html.a
-                [ href "https://ai.googleblog.com/2019/08/turbo-improved-rainbow-colormap-for.html"
-                ]
-                [ Html.text "Turbo" ]
-            ]
-        , div [ class "palette" ] (interpolation turboInterpolator)
+pallete : String -> List ( String, List (Html msg) ) -> Html msg
+pallete title items =
+    Html.section [ style "display" "flex", style "align-items" "flex-start", style "margin-bottom" "30px", style "padding-right" "20px" ]
+        [ Html.h3 [ style "position" "sticky", style "top" "10px", style "width" "250px" ] [ Html.text title ]
+        , items
+            |> List.map
+                (\( name, colors ) ->
+                    Html.div []
+                        [ Html.h5 [ style "margin-bottom" "4px", style "margin-top" "10px", style "font-weight" "normal" ] [ Html.text name ]
+                        , Html.div [ style "display" "flex" ] colors
+                        ]
+                )
+            |> Html.div [ style "flex-grow" "1" ]
         ]
 
 
-cyclic : List (Html msg)
-cyclic =
-    [ ( "rainbow", rainbowInterpolator )
-    , ( "sinebow", sinebowInterpolator )
-    ]
-        |> List.map
-            (\( title, interpolator ) ->
-                div []
-                    [ div []
-                        [ Html.text title ]
-                    , div [ class "palette" ] (interpolation interpolator)
-                    ]
-            )
-        |> (::) turboColorMap
+interpolatorPallete : String -> List ( String, Interpolator Color ) -> Html msg
+interpolatorPallete title items =
+    pallete title (List.map (Tuple.mapSecond interpolation) items)
 
 
-sequentialSingleHue : List (Html msg)
-sequentialSingleHue =
-    [ ( "blues", bluesInterpolator )
-    , ( "greens", greensInterpolator )
-    , ( "greys", greysInterpolator )
-    , ( "oranges", orangesInterpolator )
-    , ( "light-oranges", lightOrangeInterpolator )
-    , ( "purples", purplesInterpolator )
-    , ( "reds", redsInterpolator )
-    , ( "browns", brownsInterpolator )
-    , ( "teals", tealInterpolator )
-    , ( "warm greys", warmGreysInterpolator )
-    ]
-        |> List.map
-            (\( title, interpolator ) ->
-                div []
-                    [ div []
-                        [ Html.text title ]
-                    , div [ class "palette" ] (interpolation interpolator)
-                    ]
-            )
+collectionPallete : String -> List ( String, List Color ) -> Html msg
+collectionPallete title items =
+    pallete title (List.map (Tuple.mapSecond (List.map swatch)) items)
 
 
-sequentialMultiHue : List (Html msg)
-sequentialMultiHue =
-    [ ( "blue-greens", blueGreenInterpolator )
-    , ( "blue-purples", bluePurpleInterpolator )
-    , ( "green-blues", greenBlueInterpolator )
-    , ( "orange-reds", orangeRedInterpolator )
-    , ( "purple-blues", purpleBlueInterpolator )
-    , ( "purple-blue-greens", purpleBlueGreenInterpolator )
-    , ( "purple-reds", purpleRedInterpolator )
-    , ( "red-purples", redPurpleInterpolator )
-    , ( "yellow-greens", yellowGreenInterpolator )
-    , ( "yellow-orange-browns", yellowOrangeBrownInterpolator )
-    , ( "yellow-orange-reds", yellowOrangeRedInterpolator )
-    , ( "teal-bluess", tealBluesInterpolator )
-    , ( "gold-greens", goldGreensInterpolator )
-    , ( "gold-oranges", goldOrangeInterpolator )
-    , ( "gold-reds", goldRedInterpolator )
-    , ( "light-grey-reds", lightGreyRedInterpolator )
-    , ( "light-grey-teals", lightGreyTealInterpolator )
-    , ( "light-multi", lightMultiInterpolator )
-    ]
-        |> List.map
-            (\( title, interpolator ) ->
-                div []
-                    [ div []
-                        [ Html.text title ]
-                    , div [ class "palette" ] (interpolation interpolator)
-                    ]
-            )
-
-
-diverging : List (Html msg)
-diverging =
-    [ ( "carbon palette1", carbonDiverging1Interpolator )
-    , ( "carbon palette2", carbonDiverging2Interpolator )
-    , ( "blue-oranges", blueOrangeInterpolator )
-    , ( "brown-blue-greens", brownBlueGreenInterpolator )
-    , ( "purple-greens", purpleGreenInterpolator )
-    , ( "purple-oranges", purpleOrangeInterpolator )
-    , ( "red-blues", redBlueInterpolator )
-    , ( "red-greys", redGreyInterpolator )
-    , ( "yellow-green-blues", yellowGreenBlueInterpolator )
-    , ( "red-yellow-blues", redYellowBlueInterpolator )
-    , ( "red-yellow-greens", redYellowGreenInterpolator )
-    , ( "pink-yellow-greens", pinkYellowGreenInterpolator )
-    , ( "spectral", spectralInterpolator )
-    ]
-        |> List.map
-            (\( title, interpolator ) ->
-                div []
-                    [ div []
-                        [ Html.text title ]
-                    , div [ class "palette" ] (interpolation interpolator)
-                    ]
-            )
-
-
-categorical : List (Html msg)
-categorical =
-    [ ( "category10", category10 )
-    , ( "tableau10", tableau10 )
-    , ( "paired", paired )
-    , ( "set1", set1 )
-    , ( "set2", set2 )
-    , ( "accent", accent )
-    , ( "pastel1", pastel1 )
-    , ( "pastel2", pastel2 )
-    , ( "colorblind", colorblind )
-    ]
-        |> List.map
-            (\( title, colors ) ->
-                div []
-                    [ div []
-                        [ Html.text title ]
-                    , div [ class "palette" ]
-                        (colors
-                            |> List.map
-                                (\color ->
-                                    Html.div [ style "background-color" (Color.toCssString color) ] []
-                                )
-                        )
-                    ]
-            )
+swatch : Color -> Html msg
+swatch color =
+    Html.div [ style "background-color" (Color.toCssString color), title (Color.toCssString color), style "height" "30px", style "flex-grow" "1" ] []
 
 
 interpolation : Interpolator Color -> List (Html msg)
@@ -249,49 +52,74 @@ interpolation interpolator =
     List.range 1 61
         |> List.map (\v -> toFloat v / 60)
         |> List.map interpolator
-        |> List.map
-            (\color ->
-                Html.div [ style "background-color" (Color.toCssString color) ] []
-            )
+        |> List.map swatch
 
 
 view : Html msg
 view =
-    Html.div []
-        [ Html.node "style" [] [ Html.text css ]
-        , Html.div
-            [ class "wrapper" ]
-            [ div [] [ Html.h1 [] [ Html.text "Color Maps" ] ]
-            , div []
-                [ div []
-                    (Html.h2 [] [ Html.text "Categorical" ]
-                        :: categorical
-                    )
-                ]
-            , div []
-                [ div []
-                    (Html.h2 [] [ Html.text "Sequential Single-Hue" ]
-                        :: sequentialSingleHue
-                    )
-                ]
-            , div []
-                [ div []
-                    (Html.h2 [] [ Html.text "Sequential Multi-Hue" ]
-                        :: sequentialMultiHue
-                    )
-                ]
-            , div []
-                [ div []
-                    (Html.h2 [] [ Html.text "Diverging" ]
-                        :: diverging
-                    )
-                ]
-            , div []
-                [ div []
-                    (Html.h2 [] [ Html.text "Cyclic" ]
-                        :: cyclic
-                    )
-                ]
+    Html.div [ style "height" "100vh", style "overflow-y" "auto", style "font-family" "-apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, Helvetica, Arial, sans-serif" ]
+        [ collectionPallete "Categorical"
+            [ ( "category10", Scale.Color.category10 )
+            , ( "tableau10", Scale.Color.tableau10 )
+            , ( "paired", Scale.Color.paired )
+            , ( "set1", Scale.Color.set1 )
+            , ( "set2", Scale.Color.set2 )
+            , ( "accent", Scale.Color.accent )
+            , ( "pastel1", Scale.Color.pastel1 )
+            , ( "pastel2", Scale.Color.pastel2 )
+            , ( "colorblind", Scale.Color.colorblind )
+            ]
+        , interpolatorPallete "Sequential Single-Hue"
+            [ ( "blues", Scale.Color.bluesInterpolator )
+            , ( "greens", Scale.Color.greensInterpolator )
+            , ( "greys", Scale.Color.greysInterpolator )
+            , ( "oranges", Scale.Color.orangesInterpolator )
+            , ( "light-oranges", Scale.Color.lightOrangeInterpolator )
+            , ( "purples", Scale.Color.purplesInterpolator )
+            , ( "reds", Scale.Color.redsInterpolator )
+            , ( "browns", Scale.Color.brownsInterpolator )
+            , ( "teals", Scale.Color.tealInterpolator )
+            , ( "warm greys", Scale.Color.warmGreysInterpolator )
+            ]
+        , interpolatorPallete "Sequential Multi-Hue"
+            [ ( "blue-greens", Scale.Color.blueGreenInterpolator )
+            , ( "blue-purples", Scale.Color.bluePurpleInterpolator )
+            , ( "green-blues", Scale.Color.greenBlueInterpolator )
+            , ( "orange-reds", Scale.Color.orangeRedInterpolator )
+            , ( "purple-blues", Scale.Color.purpleBlueInterpolator )
+            , ( "purple-blue-greens", Scale.Color.purpleBlueGreenInterpolator )
+            , ( "purple-reds", Scale.Color.purpleRedInterpolator )
+            , ( "red-purples", Scale.Color.redPurpleInterpolator )
+            , ( "yellow-greens", Scale.Color.yellowGreenInterpolator )
+            , ( "yellow-orange-browns", Scale.Color.yellowOrangeBrownInterpolator )
+            , ( "yellow-orange-reds", Scale.Color.yellowOrangeRedInterpolator )
+            , ( "teal-bluess", Scale.Color.tealBluesInterpolator )
+            , ( "gold-greens", Scale.Color.goldGreensInterpolator )
+            , ( "gold-oranges", Scale.Color.goldOrangeInterpolator )
+            , ( "gold-reds", Scale.Color.goldRedInterpolator )
+            , ( "light-grey-reds", Scale.Color.lightGreyRedInterpolator )
+            , ( "light-grey-teals", Scale.Color.lightGreyTealInterpolator )
+            , ( "light-multi", Scale.Color.lightMultiInterpolator )
+            ]
+        , interpolatorPallete "Diverging"
+            [ ( "carbon palette1", Scale.Color.carbonDiverging1Interpolator )
+            , ( "carbon palette2", Scale.Color.carbonDiverging2Interpolator )
+            , ( "blue-oranges", Scale.Color.blueOrangeInterpolator )
+            , ( "brown-blue-greens", Scale.Color.brownBlueGreenInterpolator )
+            , ( "purple-greens", Scale.Color.purpleGreenInterpolator )
+            , ( "purple-oranges", Scale.Color.purpleOrangeInterpolator )
+            , ( "red-blues", Scale.Color.redBlueInterpolator )
+            , ( "red-greys", Scale.Color.redGreyInterpolator )
+            , ( "yellow-green-blues", Scale.Color.yellowGreenBlueInterpolator )
+            , ( "red-yellow-blues", Scale.Color.redYellowBlueInterpolator )
+            , ( "red-yellow-greens", Scale.Color.redYellowGreenInterpolator )
+            , ( "pink-yellow-greens", Scale.Color.pinkYellowGreenInterpolator )
+            , ( "spectral", Scale.Color.spectralInterpolator )
+            ]
+        , interpolatorPallete "Cyclic"
+            [ ( "rainbow", Scale.Color.rainbowInterpolator )
+            , ( "sinebow", Scale.Color.sinebowInterpolator )
+            , ( "turbo", Scale.Color.turboInterpolator )
             ]
         ]
 
